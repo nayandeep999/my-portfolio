f27094fda6b019b770dc737dc9f1053292390326
Nayandeep <nayandeepsen@gmail.com>
2024-11-09 11:58:49 +0530

    fixed card stacking effect on mobile

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -84,7 +84,7 @@ export const ProjectsSection = () => {
               key={project.title}
               className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
               style={{
-                top: `calc(64px + ${projectIndex * 40}px`,
+                top: `calc(64px + ${projectIndex * 42}px`,
               }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
